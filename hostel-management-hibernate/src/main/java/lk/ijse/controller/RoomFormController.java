1b54c008155672bcc9070b1aa38b50c7f5d3be95
Dinarbrooke <bethmij@gmail.com>
2023-08-25 22:14:11 +0530

    added reservation form

--- hostel-management-hibernate/src/main/java/lk/ijse/controller/RoomFormController.java
+++ hostel-management-hibernate/src/main/java/lk/ijse/controller/RoomFormController.java
@@ -1,4 +1,70 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+
 public class RoomFormController {
+    public AnchorPane roomPane;
+    public Circle circleUser;
+    public Label lblDate;
+    public Label lblTime;
+    public Label lblUser;
+    public AnchorPane menuPane;
+    public TextField txtID;
+    public TextField txtType;
+    public ComboBox cmbID;
+    public ComboBox cmbType;
+    public TextField txtQty;
+    public TextField txtMoney;
+    public TableView tbl;
+    public TableColumn colID;
+    public TableColumn colType;
+    public TableColumn colQty;
+    public TableColumn colMonet;
+    public TableColumn colAction;
+
+    public void menuCloseOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void payOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void roomsOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void dashbordOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void registrationOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void manageOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void reserveOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void logoutOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void settingOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void IDOnAction(ActionEvent actionEvent) {
+    }
+
+    public void typeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnClearOnAction(ActionEvent actionEvent) {
+    }
+
+    public void menuOpenOnAction(MouseEvent mouseEvent) {
+    }
 }
