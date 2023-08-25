456887e30e85ce8b97e0224d6b114bbe162616f6
Dinarbrooke <bethmij@gmail.com>
2023-08-25 15:54:43 +0530

    added registration form

--- hostel-management-hibernate/src/main/java/lk/ijse/controller/DashboardFormController.java
+++ hostel-management-hibernate/src/main/java/lk/ijse/controller/DashboardFormController.java
@@ -1,4 +1,53 @@
 package lk.ijse.controller;
 
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+
 public class DashboardFormController {
+    public AnchorPane dashPane;
+    public Circle circleUser;
+    public Label lblDate;
+    public Label lblTime;
+    public Label lblUser;
+    public Label lblStudent;
+    public Label lblRoom;
+    public Label lblUsed;
+    public Label lblRemain;
+    public Label lblNoAcTotal;
+    public Label lblNoAcAvailble;
+    public Label lblNoAcFoodTotal;
+    public Label lblNoAcFoodAvailble;
+    public Label lblAcTotal;
+    public Label lblAcAvailble;
+    public Label lblAcFoodTotal;
+    public Label lblAcFoodAvailble;
+
+    public void dashbordOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void registrationOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void manageOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void roomsOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void reserveOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void settingOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void logoutOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void menuOpenOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void menuCloseOnAction(MouseEvent mouseEvent) {
+    }
 }
