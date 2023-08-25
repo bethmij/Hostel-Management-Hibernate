a3a4a7850a17905960d85ef03a0aeaf75a90ede4
Dinarbrooke <bethmij@gmail.com>
2023-08-25 13:02:14 +0530

    modified login form

--- hostel-management-hibernate/src/main/java/lk/ijse/controller/LoginFormController.java
+++ hostel-management-hibernate/src/main/java/lk/ijse/controller/LoginFormController.java
@@ -1,4 +1,15 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginFormController {
+    public AnchorPane logPane;
+
+    public void logInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void signUpOnAction(MouseEvent mouseEvent) {
+    }
 }
