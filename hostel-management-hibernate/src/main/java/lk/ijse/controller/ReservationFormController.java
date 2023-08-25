abdba16f9faf565a1d3c0b0d7b5c70866d69f898
Dinarbrooke <bethmij@gmail.com>
2023-08-25 22:53:56 +0530

    modified reservation form

--- hostel-management-hibernate/src/main/java/lk/ijse/controller/ReservationFormController.java
+++ hostel-management-hibernate/src/main/java/lk/ijse/controller/ReservationFormController.java
@@ -1,2 +1,61 @@
-package lk.ijse.controller;public class ReservationFormController {
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+
+public class ReservationFormController {
+    public AnchorPane reservePane;
+    public Circle circleUser;
+    public Label lblDate;
+    public Label lblTime;
+    public Label lblUser;
+    public AnchorPane menuPane;
+    public TextField txtPay;
+    public ComboBox cmbRoomID;
+    public ComboBox cmbStuID;
+    public Label lblRoomType;
+    public Label lblName;
+    public Label lblAvailable;
+    public Label lblMoney;
+    public RadioButton rdPayNow;
+    public RadioButton edPayLater;
+
+    public void payOnAction(ActionEvent mouseEvent) {
+    }
+
+    public void roomsOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void dashbordOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void registrationOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void manageOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void reserveOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void logoutOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void settingOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void menuCloseOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void menuOpenOnAction(MouseEvent mouseEvent) {
+    }
 }
