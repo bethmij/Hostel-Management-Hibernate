65b4e717119e6f3428b9afb7e36edf3c6cc3f79c
Dinarbrooke <bethmij@gmail.com>
2023-08-26 00:15:42 +0530

    modified setting form

--- hostel-management-hibernate/src/main/java/lk/ijse/controller/SettingFormController.java
+++ hostel-management-hibernate/src/main/java/lk/ijse/controller/SettingFormController.java
@@ -1,4 +1,59 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+
 public class SettingFormController {
+    public AnchorPane settingPane;
+    public Circle circleUser;
+    public Label lblDate;
+    public Label lblTime;
+    public Label lblUser;
+    public AnchorPane menuPane;
+    public Circle circleUser1;
+    public Label lblUserName;
+    public TextField txtUserName;
+    public PasswordField txtPass;
+    public PasswordField txtReEnter;
+
+    public void payOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void roomsOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void dashbordOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void registrationOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void manageOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void reserveOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void logoutOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void settingOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void userOnAction(ActionEvent actionEvent) {
+    }
+
+    public void passOnAction(ActionEvent actionEvent) {
+    }
+
+    public void menuCloseOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void menuOpenOnAction(MouseEvent mouseEvent) {
+    }
 }
