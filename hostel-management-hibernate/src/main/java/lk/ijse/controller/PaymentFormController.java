30d8816ab24217331c8a45e9a6f97c47ece06768
Dinarbrooke <bethmij@gmail.com>
2023-08-25 23:23:42 +0530

    modified payment form

--- hostel-management-hibernate/src/main/java/lk/ijse/controller/PaymentFormController.java
+++ hostel-management-hibernate/src/main/java/lk/ijse/controller/PaymentFormController.java
@@ -1,4 +1,69 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+
 public class PaymentFormController {
+    public AnchorPane payPane;
+    public Circle circleUser;
+    public Label lblDate;
+    public Label lblTime;
+    public Label lblUser;
+    public AnchorPane menuPane;
+    public TextField txtName;
+    public CheckBox cbPaid;
+    public CheckBox cbNonPaid;
+    public CheckBox cbOther;
+    public TableView tbl;
+    public TableColumn colReserveID;
+    public TableColumn colStuID;
+    public TableColumn colName;
+    public TableColumn colRoomID;
+    public TableColumn colType;
+    public TableColumn colStatus;
+    public TableColumn colRemain;
+    public TableColumn colAction;
+    public TableColumn colAction1;
+
+    public void menuCloseOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void payOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void roomsOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void dashbordOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void registrationOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void manageOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void reserveOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void logoutOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void settingOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnGetAllOnAction(ActionEvent actionEvent) {
+    }
+
+    public void menuOpenOnAction(MouseEvent mouseEvent) {
+    }
 }
