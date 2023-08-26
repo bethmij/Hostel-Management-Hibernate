5090094e2404bae49b3de9662a6f4f370f24917e
Dinarbrooke <bethmij@gmail.com>
2023-08-26 09:54:16 +0530

    modified

--- hostel-management-hibernate/src/main/java/lk/ijse/controller/DashboardFormController.java
+++ hostel-management-hibernate/src/main/java/lk/ijse/controller/DashboardFormController.java
@@ -1,11 +1,16 @@
 package lk.ijse.controller;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
+import lk.ijse.dao.custom.impl.util.OpenView;
 
-public class DashboardFormController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardFormController implements Initializable {
     public AnchorPane dashPane;
     public Circle circleUser;
     public Label lblDate;
@@ -25,33 +30,50 @@ public class DashboardFormController {
     public Label lblAcFoodAvailble;
     public AnchorPane menuPane;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        menuPane.setVisible(false);
+    }
+
     public void dashbordOnAction(MouseEvent mouseEvent) {
+        OpenView.openView("dashboardForm",dashPane);
     }
 
     public void registrationOnAction(MouseEvent mouseEvent) {
+        OpenView.openView("registrationForm",dashPane);
     }
 
     public void manageOnAction(MouseEvent mouseEvent) {
+        OpenView.openView("studentManageForm",dashPane);
     }
 
     public void roomsOnAction(MouseEvent mouseEvent) {
+        OpenView.openView("roomForm",dashPane);
     }
 
     public void reserveOnAction(MouseEvent mouseEvent) {
+        OpenView.openView("reservationForm",dashPane);
     }
 
     public void settingOnAction(MouseEvent mouseEvent) {
+        OpenView.openView("settingForm",dashPane);
     }
 
     public void logoutOnAction(MouseEvent mouseEvent) {
+        OpenView.openView("loginForm",dashPane);
     }
 
     public void menuOpenOnAction(MouseEvent mouseEvent) {
+        menuPane.setVisible(true);
     }
 
     public void menuCloseOnAction(MouseEvent mouseEvent) {
+        menuPane.setVisible(false);
     }
 
     public void payOnAction(MouseEvent mouseEvent) {
+        OpenView.openView("paymentForm",dashPane);
     }
+
+
 }
--- hostel-management-hibernate/src/main/java/lk/ijse/controller/LoginFormController.java
+++ hostel-management-hibernate/src/main/java/lk/ijse/controller/LoginFormController.java
@@ -3,11 +3,13 @@ package lk.ijse.controller;
 import javafx.event.ActionEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.dao.custom.impl.util.OpenView;
 
 public class LoginFormController {
     public AnchorPane logPane;
 
     public void logInOnAction(ActionEvent actionEvent) {
+        OpenView.openView("dashboardForm",logPane);
     }
 
     public void signUpOnAction(MouseEvent mouseEvent) {
